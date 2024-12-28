9061888aafeefb1d2073772fe2190f68576fe8bf
Dewmith Akalanka <akalankadewmith@gmail.com>
2024-12-28 16:49:27 +0530

    add technologies

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+          <div className="flex flex-col items-center">
+            <p className="text-center">{technology.name}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Tech
+export default SectionWrapper(Tech, "");
