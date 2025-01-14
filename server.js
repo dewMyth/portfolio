74b6b00f91be77772660147b1247158fdf980d0b
Dewmith Akalanka <akalankadewmith@gmail.com>
2025-01-14 18:33:02 +0530

    change dirname not defined issue

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import path from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, "dist")));
